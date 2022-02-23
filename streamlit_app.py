0efef1db145e9ef289a3a411cf0f1ca02dfed1b1
Yining Wang <yiningw4@andrew.cmu.edu>
2022-02-23 13:14:41 -0500

    finished load_data and data description

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,26 +7,36 @@ st.title("Let's analyze some Penguin Data 🐧📊.")
 @st.cache  # add caching so we load the data only once
 def load_data():
     # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+    weather_path = "/Users/xmnora/Desktop/assignment-2-bingdwendwen/weather.csv"
+    return pd.read_csv(weather_path)
 
 df = load_data()
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
+st.text("The dataset has the following columns : \n"
+ + "PRCP = Precipitation (inch) \n"
+ + "SNOW = Snowfall (inch) \n"
+ + "SNWD = Snow depth (inch) \n"
+ + "TMAX = Maximum temperature (F) \n" 
+ + "TMIN = Minimum temperature (F) \n"
+ + "TAVG = Average temperature (F) \n"
+ + "AWND = Average daily wind speed (miles / hour)\n" 
+ + "WSF5 = Fastest 5-second wind speed (miles / hour)\n" 
+ + "WDF5 = Direction of fastest 5-second wind (degrees)\n")
+
 
 st.write(df)
 
 st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
-).properties(
-    width=600, height=400
-).interactive()
+# chart = alt.Chart(df).mark_point().encode(
+#     x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
+#     y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
+#     color=alt.Y("species")
+# ).properties(
+#     width=600, height=400
+# ).interactive()
 
-st.write(chart)
+# st.write(chart)
 
-st.markdown("This project was created by Student1 and Student2 for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
+# st.markdown("This project was created by Student1 and Student2 for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
 
