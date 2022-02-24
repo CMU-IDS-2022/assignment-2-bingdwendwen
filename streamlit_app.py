65dfef7de58418ec18db832bdd67fd72e5295c2b
Yining Wang <yiningw4@andrew.cmu.edu>
2022-02-24 00:29:34 -0500

    finished loading data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,59 @@
+from turtle import color
+
+from numpy import sort
 import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("Let's analyze some Yelp Business Data.")
 
 @st.cache  # add caching so we load the data only once
 def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    weather_path = "/Users/xmnora/Desktop/assignment-2-bingdwendwen/weather.csv"
-    return pd.read_csv(weather_path)
+    business_path = "yelp_business_cleaned.csv"
+    df = pd.read_csv(business_path).astype(str)
+    return df
 
 df = load_data()
-
-st.text("The dataset has the following columns : \n"
- + "PRCP = Precipitation (inch) \n"
- + "SNOW = Snowfall (inch) \n"
- + "SNWD = Snow depth (inch) \n"
- + "TMAX = Maximum temperature (F) \n" 
- + "TMIN = Minimum temperature (F) \n"
- + "TAVG = Average temperature (F) \n"
- + "AWND = Average daily wind speed (miles / hour)\n" 
- + "WSF5 = Fastest 5-second wind speed (miles / hour)\n" 
- + "WDF5 = Direction of fastest 5-second wind (degrees)\n")
-
 
 st.write(df)
 
 st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
 
-# chart = alt.Chart(df).mark_point().encode(
-#     x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-#     y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-#     color=alt.Y("species")
-# ).properties(
-#     width=600, height=400
-# ).interactive()
 
-# st.write(chart)
+# df_map = [pd.to_numeric(df['latitude']),pd.to_numeric(df['longitude'])]
+# st.map(df_map)
 
-# st.markdown("This project was created by Student1 and Student2 for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
+
+df['stars'] = pd.to_numeric(df['stars'])
+df['review_count'] = pd.to_numeric(df['review_count'])
+
+review_count_chart = alt.Chart(df).mark_line().encode(
+    x=alt.X("review_count", scale=alt.Scale(zero=False), sort="-x"),
+    y=alt.Y("mean(stars)", scale=alt.Scale(zero=False)),
+    color = 'count(stars)',
+).properties(
+    width=800, height=400
+).interactive()
+
+st.write(review_count_chart)
+
+
+
+chart = alt.Chart(df).mark_bar().encode(
+    x=alt.X("state", scale=alt.Scale(zero=False), sort="-x"),
+    y=alt.Y("mean(stars)", scale=alt.Scale(zero=False)),
+    color = 'count(stars)',
+    tooltip = ['count(stars)','median(stars)', 'max(stars)', 'min(stars)' ]
+).properties(
+    width=800, height=400
+).interactive()
+
+st.write(chart)
+
+
+
+
+
+
+st.markdown("This project was created by Yining Wang and Jiaxiang Wu for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
 
